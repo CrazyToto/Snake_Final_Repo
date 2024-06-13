da08f4f2b602bf4bc2904f85de1deea109e10965
Tom Arnsberger <167811950+tom4rn@users.noreply.github.com>
2024-06-13 21:44:23 +0200

    Update Apple.cs

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -1,39 +1,39 @@
 using System.Collections;           //Impotiert Lybarys
 using System.Collections.Generic;   //
-using UnityEngine;                  //Lybary für Unity 
+using UnityEngine;                  //Lybary fÃ¼r Unity 
 
-public class Apple : MonoBehaviour   //Klasse Apple // MonoBehviour für Implementierung in Unity mit Start und Setup Funktionen
+public class Apple : MonoBehaviour   //Klasse Apple // MonoBehviour fÃ¼r Implementierung in Unity mit Start und Setup Funktionen
 {
   public BoxCollider2D gridArea;    //Variable gridArea vom Typ BoxCollider2D, dies ist die Triggerbox
     public int score;   //Variable score vom Typ int
 
 
-    private void Start() // wird beim Start des Programms einmal ausgeführt
+    private void Start() // wird beim Start des Programms einmal ausgefÃ¼hrt
     {
-      RandomizePosition(); // Funktion für das Setzten der Position des Essen
+      RandomizePosition(); // Funktion fÃ¼r das Setzten der Position des Essen
         score = 0;  // legt den Startwert des Scores fest
     }
-    private void RandomizePosition() // Funktion für das Setzten der Position des Essen
+    private void RandomizePosition() // Funktion fÃ¼r das Setzten der Position des Essen
     {
         Bounds bounds = this.gridArea.bounds;   // legt die Grenzen des Spawnfeldes fest
 
-        float x = Random.Range(bounds.min.x, bounds.max.x); // legt die x-Position auf einen zufälligen Wert im Rahmen fest
-        float y = Random.Range(bounds.min.y, bounds.max.y); // legt die y-Position auf einen zufälligen Wert im Rahmen fest
+        float x = Random.Range(bounds.min.x, bounds.max.x); // legt die x-Position auf einen zufÃ¤lligen Wert im Rahmen fest
+        float y = Random.Range(bounds.min.y, bounds.max.y); // legt die y-Position auf einen zufÃ¤lligen Wert im Rahmen fest
 
         this.transform.position = new Vector3(Mathf.Round(x),Mathf.Round(y), 0.0f); // legt die Position fest;
-                                                                                    // Mathf.Round rundet die Ergebnisse, sodass die Blöcke alle
-                                                                                    // auf dem Koordinatensystem auf gleicher Höhe sind
+                                                                                    // Mathf.Round rundet die Ergebnisse, sodass die BlÃ¶cke alle
+                                                                                    // auf dem Koordinatensystem auf gleicher HÃ¶he sind
     
     
     }
 
 
 
-    private void OnTriggerEnter2D(Collider2D other) // Funktion für die Kollision mit dem Schlangenkopf
+    private void OnTriggerEnter2D(Collider2D other) // Funktion fÃ¼r die Kollision mit dem Schlangenkopf; wird ausgefÃ¼hrt bei Kollision des Box Collider
     {   if (other.tag == "Player") // wenn das Objekt mit dem kollidiert wird den Tag Player hat, diesen hat der Schlangenkopf
         {
             RandomizePosition(); // erscheint das Essen wieder an einem neuen Ort 
-            score = score + 1;  // der Score wird um 1 erhöht
+            score = score + 1;  // der Score wird um 1 erhÃ¶ht
         }
     }
 }
